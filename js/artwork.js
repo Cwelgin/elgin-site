a81ab18d5d2dd3ce06bee88862cf2857dee02ebd
Cwelgin <Cwelgin@gmail.com>
2026-07-28 12:38:19 -0700

    Create artwork.js

---
+++ js/artwork.js
@@ -0,0 +1,17 @@
+/*
+    artwork.js
+
+    Shared artwork rendering library for cliffelgin.com
+
+    Functions
+
+    Artwork.init()
+    Artwork.read()
+    Artwork.build()
+    Artwork.renderMetadata()
+    Artwork.loadHeroImage()
+    Artwork.setupLightbox()
+    Artwork.loadGallery()
+    Artwork.loadRelated()
+
+*/
